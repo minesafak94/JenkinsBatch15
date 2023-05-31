26446883493e031b38806e223be18b08d389d07d
minesafak94 <minesafak94@gmail.com>
2023-05-30 20:16:34 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello world");
+        for (int i=0; i <10; i++){
+            System.out.println("Hello World");
+            Thread.sleep(2000);
+        }
+    }
+}
